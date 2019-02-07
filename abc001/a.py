f656fb56388fbb2aded0ef7ad93836c7c7f19ca6
nabeen <watanabe_kenichiro@hasigo.co.jp>
2019-02-08 01:38:50 +0900

    ABC001 A問題 Python

---
+++ abc001/a.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# https://atcoder.jp/contests/abc001/tasks/abc001_1
+
+
+def main() -> None:
+    fir = int(input())
+    sec = int(input())
+    print(fir - sec)
+
+
+if __name__ == '__main__':
+    main()
