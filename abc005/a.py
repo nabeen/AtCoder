d6663b9b56aed869d78dd4be4b0710bee5e43ea7
nabeen <watanabe_kenichiro@hasigo.co.jp>
2019-02-11 16:01:03 +0900

    ABC005 A問題B問題

---
+++ abc005/a.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# https://atcoder.jp/contests/abc005/tasks/abc005_1
+
+
+def main() -> None:
+    x, y = map(int, input().split())
+    print("%s" % (y // x))
+
+
+if __name__ == '__main__':
+    main()
