763d4523acaa7fdc5e2b588345e4d701b607e850
nabeen <watanabe_kenichiro@hasigo.co.jp>
2019-02-13 00:09:18 +0900

    ABC007 A問題B問題

---
+++ abc007/a.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# https://atcoder.jp/contests/abc007/tasks/abc007_1
+
+
+def main() -> None:
+    x = int(input())
+    print(x-1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ abc007/b.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# https://atcoder.jp/contests/abc007/tasks/abc007_2
+
+
+def main() -> None:
+    x = input()
+    print("%s" % "-1" if x == "a" else "a")
+
+
+if __name__ == '__main__':
+    main()
